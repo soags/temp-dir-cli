2663f13da13606640407fab612add9b32aafa9e1
soags <17847282+soags@users.noreply.github.com>
2024-07-22 10:25:28 +0900

    Safely path args

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -5,7 +5,7 @@ export function cleanup(parentPath: string) {
   fs.readdirSync(parentPath, { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
     .forEach((dirent) => {
-      const directoryPath = path.join(dirent.parentPath, dirent.name)
+      const directoryPath = path.join(parentPath, dirent.name)
 
       const files = fs.readdirSync(directoryPath)
       if (files.length === 0) {
